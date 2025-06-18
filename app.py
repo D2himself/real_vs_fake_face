719d995df14422f666fd6c25cccd7179fee35f8a
D2himself <omokemiayodimeji@gmail.com>
2025-06-18 22:23:38 +0100

    Fix app.py again

--- app.py
+++ app.py
@@ -4,10 +4,8 @@ from fastai.metrics import F1Score, Precision, Recall
 from huggingface_hub import hf_hub_download
 import gradio as gr
 from pathlib import Path
-from shutil import copyfiler
-from fastai.vision.widgets import *
-from fastbook import *
-import matplotlib.pyplot as plt
+from shutil import copyfile
+
 
 model_path = hf_hub_download(
     repo_id="Omokemi/real-vs-ai-model",  
