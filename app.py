83b21983df4c91b728baf684d88f945d6fe087e0
D2himself <omokemiayodimeji@gmail.com>
2025-06-18 22:15:44 +0100

    Fix numpy version and import custom classes

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ from fastai.callback.mixup import MixUp
 from fastai.metrics import F1Score, Precision, Recall
 from huggingface_hub import hf_hub_download
 import gradio as gr
-
+from pathlib import Path
+from shutil import copyfiler
+from fastai.vision.widgets import *
+from fastbook import *
+import matplotlib.pyplot as plt
 
 model_path = hf_hub_download(
     repo_id="Omokemi/real-vs-ai-model",  
